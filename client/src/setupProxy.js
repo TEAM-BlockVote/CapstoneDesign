df254c22f3a423143be326099091259f4ce7f106
{NSComman} <yunsung2015@gmail.com>
2023-05-17 19:48:34 +0900

    회원가입 기능 및 코드 리페어

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -14,4 +14,11 @@ module.exports = function (app) {
       changeOrigin: true,
     })
   );
+
+  app.use(
+    createProxyMiddleware('/signup', {
+      target: 'http://localhost:5000/',
+      changeOrigin: true,
+    })
+  )
 };
