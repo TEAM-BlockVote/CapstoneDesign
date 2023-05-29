cbdec21e16e1dd95fb3c33f619d91947148da5e6
{NSComman} <yunsung2015@gmail.com>
2023-05-29 11:53:24 +0900

    나후찾 메인 연결

---
+++ client/src/Components/Nawoo/NawooCategory.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import "./NawooCategory.css";
+
+function NawooCategory() {
+  return (
+    <div className='nawoo_form'>
+      <div className='category_top'>
+        <label className='category_label1'>누가 당신과 적합한 후보입니까?</label>
+        <label className='category_label2'>선택하시오.</label>
+      </div>
+      <div className='category_bottom'>
+
+      </div>
+    </div>
+  );
+};
+export default NawooCategory;
--- client/src/Components/Nawoo/NawooPage.js
+++ client/src/Components/Nawoo/NawooPage.js
@@ -1,12 +1,14 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import NawooMain from './NawooMain';
+import NawooCategory from './NawooCategory';
+import NawooQna from './NawooQna';
 import "./NawooPage.css";
 
 function NawooPage() {
+  
   return (
     <div className='nawoo_site'>
       <div className='nawoo_main'>
-        <NawooMain/>
       </div>
     </div>
   );
