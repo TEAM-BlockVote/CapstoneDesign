98e1b3dce15059d1ee1b47d21905a31899c17374
{NSComman} <yunsung2015@gmail.com>
2023-06-01 13:26:30 +0900

    나후찾 카테고리 기능 개발

--- client/src/Components/Nawoo/NawooCategory.js
+++ client/src/Components/Nawoo/NawooCategory.js
@@ -3,15 +3,15 @@ import "./NawooCategory.css";
 
 function NawooCategory({ onButtonClick }) {
   const [count, setCount] = useState(0);
-  const [selectedCategory, setSelectedCategory] = useState('');
+  const [selectedCategories, setSelectedCategories] = useState([]);
 
   const handleButtonClick = (category) => {
-    if (selectedCategory === category) {
+    if (selectedCategories.includes(category)) {
       setCount(count - 10);
-      setSelectedCategory('');
+      setSelectedCategories(selectedCategories.filter((selectedCategory) => selectedCategory !== category));
     } else {
       setCount(count + 10);
-      setSelectedCategory(category);
+      setSelectedCategories([...selectedCategories, category]);
     }
   };
 
@@ -20,46 +20,67 @@ function NawooCategory({ onButtonClick }) {
       <div className='category_top'>
         <label className='category_label1'>누가 당신과 적합한 후보입니까?</label>
         <label className='category_label2'>선택하시오.</label>
-        <div className='category_count'>
-          {count}
+        <div className='count_margin'>
+          <label className='category_label3'>문항 : </label>
+          <div className='category_count'>
+            {count}
+          </div>
         </div>
       </div>
       <div className='category_middle'>
         <button
-          className={`category_btn ${selectedCategory === '시설' ? 'selected' : ''}`}
+          className={`category_btn ${selectedCategories.includes('시설') ? 'selected' : ''}`}
           onClick={() => handleButtonClick('시설')}
         >
           시설
         </button>
         <button
-          className={`category_btn ${selectedCategory === '복지' ? 'selected' : ''}`}
+          className={`category_btn ${selectedCategories.includes('복지') ? 'selected' : ''}`}
           onClick={() => handleButtonClick('복지')}
         >
           복지
         </button>
         <button
-          className={`category_btn ${selectedCategory === '교육' ? 'selected' : ''}`}
+          className={`category_btn ${selectedCategories.includes('교육') ? 'selected' : ''}`}
           onClick={() => handleButtonClick('교육')}
         >
           교육
         </button>
         <button
-          className={`category_btn ${selectedCategory === '소통' ? 'selected' : ''}`}
+          className={`category_btn ${selectedCategories.includes('소통') ? 'selected' : ''}`}
           onClick={() => handleButtonClick('소통')}
         >
           소통
         </button>
         <button
-          className={`category_btn ${selectedCategory === '행사' ? 'selected' : ''}`}
+          className={`category_btn ${selectedCategories.includes('행사') ? 'selected' : ''}`}
           onClick={() => handleButtonClick('행사')}
         >
           행사
         </button>
         <button
-          className={`category_btn ${selectedCategory === '진로' ? 'selected' : ''}`}
+          className={`category_btn ${selectedCategories.includes('진로') ? 'selected' : ''}`}
           onClick={() => handleButtonClick('진로')}
         >
           진로
+        </button>
+        <button
+          className={`category_btn ${selectedCategories.includes('생활') ? 'selected' : ''}`}
+          onClick={() => handleButtonClick('생활')}
+        >
+          생활
+        </button>
+        <button
+          className={`category_btn ${selectedCategories.includes('의료') ? 'selected' : ''}`}
+          onClick={() => handleButtonClick('의료')}
+        >
+          의료
+        </button>
+        <button
+          className={`category_btn ${selectedCategories.includes('상담') ? 'selected' : ''}`}
+          onClick={() => handleButtonClick('상담')}
+        >
+          상담
         </button>
       </div>
       <div className='category_bottom'>
