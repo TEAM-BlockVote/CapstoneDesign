9474ad5268614d723c9dec2420a56d1ac477e59f
LSJ24KR <127313368+LSJ24KR@users.noreply.github.com>
2023-06-02 14:21:47 +0900

    no message

--- sql/qna.sql
+++ sql/qna.sql
@@ -2,7 +2,8 @@ CREATE TABLE `blockvote`.`qna` (
   `title` VARCHAR(45) NOT NULL,
   `name` VARCHAR(45) NOT NULL,
   `date` VARCHAR(45) NOT NULL,
-  `view` int DEFAULT NOT NULL,
+  `view` int DEFAULT '0' NOT NULL,
   `content` VARCHAR(500) NOT NULL,
-  PRIMARY KEY (`title`)
+  'id' int NOT NULL,
+  PRIMARY KEY (`id`)
 );
