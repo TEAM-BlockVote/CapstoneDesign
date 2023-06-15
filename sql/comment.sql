ecc7a7d3d1926071bf6ef1eafe335b84a5028af6
jhchon <jjh5999@naver.com>
2023-06-16 01:58:03 +0900

    댓글 디비 studentNumber 컬럼 추가

--- sql/comment.sql
+++ sql/comment.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `comment` (
   `id` INT NOT NULL,
   `text` VARCHAR(150) NOT NULL,
+  `studentNumber` VARCHAR(150) NOT NULL,
   CONSTRAINT FK_comment_qna FOREIGN KEY (id) 
   REFERENCES qna (id)
 );
