04da1012e4f77feea126d540f62391e459b83a79
jhchon <31531119+jhchon@users.noreply.github.com>
2023-07-14 06:07:54 +0900

    블록체인 배포/환경설정

---
+++ scripts/deploy.js
@@ -0,0 +1,13 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  const Voting = await ethers.getContractFactory("Voting");
+  const voting = await Voting.deploy();
+  await voting.deployed();
+  console.log("Voting contract deployed to:", voting.address);
+}
+
+main().then(() => process.exit(0)).catch(error => {
+  console.error(error);
+  process.exit(1);
+});
