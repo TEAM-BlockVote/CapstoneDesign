09d62a897965091620da484377fbd88cf1c72cb2
jhchon <31531119+jhchon@users.noreply.github.com>
2023-07-14 06:03:45 +0900

    유저 지갑 컬럼 생성

--- sql/uesrs.sql
+++ sql/uesrs.sql
@@ -7,5 +7,7 @@ CREATE TABLE `users` (
   `telNumber` VARCHAR(30) NULL,
   `snsId` VARCHAR(100) NULL,
   `verificationStatus` VARCHAR(30) DEFAULT 'true',
+  `walletAddr` VARCHAR(100) NOT NULL,
+  `walletPrivateKey` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`studentNumber`)
 );
