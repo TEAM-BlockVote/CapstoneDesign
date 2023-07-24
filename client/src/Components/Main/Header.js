9933f353e297070e2e1114c3c6cfa776199aaa50
jhchon <31531119+jhchon@users.noreply.github.com>
2023-07-24 23:46:55 +0900

    유저 프로필 모달창 생성

--- client/src/Components/Main/Header.js
+++ client/src/Components/Main/Header.js
@@ -3,71 +3,64 @@ import React, { useContext } from 'react';
 import logo from '../Main/images/logo.png';
 import AuthContext from '../../Store/auth-context';
 
-import CloseButton from 'react-bootstrap/CloseButton';
-import Modal from 'react-bootstrap/Modal';
-
 import { useState } from 'react';
+
+import InfoModal from './InfoModal';
 
 const Header = () => {
   const ctx = useContext(AuthContext);
-  const [showVotingModal, setShowVotingModal] = useState(false);
-  const handleVotingModalClose = () => setShowVotingModal(false);
-  const handleVotingModalShow = () => setShowVotingModal(true);
+  const [modalOpen, setModalOpen] = useState(false);
+  const showModal = () => {
+    setModalOpen(true);
+  };  
 
   return (
-    <>
-    <nav className="navbar navbar-expand-lg" style={{margin: '0 5%'}}>
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/"><img  style={{width:'200px'}} src={logo} alt="seoilLogo"/></Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-          style={{marginLeft: 'auto', marginRight: '1rem'}}
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse justify-start" id="navbarSupportedContent">
-          <ul className="navbar-nav m-auto">
-            <li className="nav-item me-5">
-              <Link className="nav-link" to="/AdminMain"> 투표 만들기 </Link>
-            </li>
-            <li className="nav-item me-5">
-              <Link className="nav-link" to="/notice">  QnA </Link>
-            </li>
-            <li className="nav-item me-5">
-              <Link className="nav-link" to="/NawooPage"> 나만의 후보찾기 </Link>
-            </li>
-          </ul>
-          <ul className="navbar-nav account">
-            <li className="nav-item">
-              { ctx.isLoggedIn === null && " " }
-              { ctx.isLoggedIn === false && <Link className="nav-link" to="/signIn"> 로그인 </Link> }
-            </li>
-          </ul>
-        </div>
-        { ctx.isLoggedIn === true && (
-          <div className="navbar-nav" onClick={handleVotingModalShow} style={{cursor:'pointer', color: '#0084ff'}}>
-            {ctx.userName}님
+    <div>
+      <nav className="navbar navbar-expand-lg" style={{margin: '0 5%'}}>
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/"><img  style={{width:'200px'}} src={logo} alt="seoilLogo"/></Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+            style={{marginLeft: 'auto', marginRight: '1rem'}}
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse justify-start" id="navbarSupportedContent">
+            <ul className="navbar-nav m-auto">
+              <li className="nav-item me-5">
+                <Link className="nav-link" to="/AdminMain"> 투표 만들기 </Link>
+              </li>
+              <li className="nav-item me-5">
+                <Link className="nav-link" to="/notice">  QnA </Link>
+              </li>
+              <li className="nav-item me-5">
+                <Link className="nav-link" to="/NawooPage"> 나만의 후보찾기 </Link>
+              </li>
+            </ul>
+            <ul className="navbar-nav account">
+              <li className="nav-item">
+                { ctx.isLoggedIn === null && " " }
+                { ctx.isLoggedIn === false && <Link className="nav-link" to="/signIn"> 로그인 </Link> }
+              </li>
+            </ul>
           </div>
-        )}
-        </div>
-      
-    </nav>
-
-    <Modal  show={showVotingModal} onHide={handleVotingModalClose}>
-      <Modal.Header>
-        <CloseButton onClick={handleVotingModalClose} />
-      </Modal.Header>
-      <Modal.Body>
-      { ctx.isLoggedIn === true && <div className="nav-link" onClick={ctx.logout} style={{cursor:'pointer'}}> 로그아웃 </div> }
-      </Modal.Body>
-    </Modal>
-  </>    
+          { ctx.isLoggedIn === true && (
+            <div className="navbar-nav" onClick={showModal} style={{cursor:'pointer', color: '#0084ff'}}>
+              {ctx.userName}님
+            </div>
+          )}
+          </div>
+      </nav>
+      <div>
+        {modalOpen && <InfoModal setModalOpen={setModalOpen} />}
+      </div>
+    </div>
   );
 };
 
---
+++ client/src/Components/Main/InfoModal.js
@@ -0,0 +1,36 @@
+import './InfoModal.css';
+import img from './images/face.png';
+import { useContext } from 'react';
+import AuthContext from '../../Store/auth-context';
+
+function InfoModal(props) {
+  const ctx = useContext(AuthContext);
+
+  const closeModal = () => {
+    props.setModalOpen(false);
+  };
+
+  return (
+      <div className="container22323">
+        <div className="modal_header">
+          <button className="close" onClick={closeModal}> X </button>
+          <span className='modal_profile'> 내 프로필 </span>
+        </div>
+
+        <div className="modal_body">
+          <div className='modal_img'>
+            <img src={img} alt='profile'/>
+            </div>
+          <div className='modal_name'>{ctx.userName}님</div>
+        </div>
+        <div className='modal_buttons'>
+          <p className='modal_button_blue'>학번QR코드</p>
+          <p className='modal_button_white'>블록체인계정</p>
+        </div>
+        <div className="modal_footer" onClick={() => { ctx.logout();closeModal(); }}>
+          <span className="modal_footer_logout" > 로그아웃</span>
+        </div>
+      </div>
+  );
+}
+export default InfoModal;
