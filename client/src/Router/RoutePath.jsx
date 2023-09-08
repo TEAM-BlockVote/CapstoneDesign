70f2c3e2549e25d240bc7116adc0286218f3d2fb
jhchon <31531119+jhchon@users.noreply.github.com>
2023-09-09 01:51:44 +0900

    style: [nawoo] 파일명 변경

--- client/src/Router/RoutePath.jsx
+++ client/src/Router/RoutePath.jsx
@@ -7,7 +7,7 @@ import WritingForm from "../Components/NoticeBoard/WriteAll/WritingForm";
 import AdminMain from "../Components/AdminPage/AdminMain";
 import SetVote from "../Components/AdminPage/SetVote";
 import AdditionalInfo from "../Components/Register/AdditionalInfo";
-import NawooPage from "../Components/Nawoo/NawooPage";
+import NawooMain from "../Components/Nawoo/NawooIndex";
 import VotingMain from "../Components/Vote/VotingMain";
 import QnaPosted from "../Components/NoticeBoard/TableAll/QnaPosted";
 const router = createBrowserRouter([
@@ -26,7 +26,7 @@ const router = createBrowserRouter([
     ],
   },
   {path: '/additionalInfo', element: <AdditionalInfo/>},
-  {path: '/nawoopage', element: <NawooPage/>},
+  {path: '/nawoopage', element: <NawooMain/>},
 ]);
 
 export default router;
