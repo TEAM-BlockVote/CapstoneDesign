356c72e553e28779fdb8bf64158a4ccffe9b43e6
jhchon <31531119+jhchon@users.noreply.github.com>
2023-09-15 02:35:06 +0900

    refactor: [nawoo] 더미 코드 삭제/ 디자인변경

---
+++ client/src/Components/Nawoo/Index.js
@@ -0,0 +1,47 @@
+import touchButton from '../Main/images/touchButton.png';
+import React, { useEffect } from 'react';
+import "./Index.css";
+
+const Index = ({setCurrentPage}) => {
+  const handleElementClick = (event) => {
+    const nwForm = document.querySelector('.nw_form');
+    if (nwForm.contains(event.target)) {
+      setCurrentPage("NextPage")
+    }
+  };
+  useEffect(() => {
+    document.addEventListener('click', handleElementClick);
+    return () => {
+      document.removeEventListener('click', handleElementClick);
+    };
+  }, []);
+  return (
+    <div className='nw_form'>
+      <div className='nw_top'>
+        <div className='nw_wrapper'>
+          <label className='nw_header1'>나만의 후보 찾기</label>
+           <div className="mask"><label className="text">나만의 후보 찾기</label></div>
+          <label className='nw_label2'>
+            <p className='nw_text1'>공약을 한눈에 정리해 놓은곳은 없을까?</p>
+            <p className='nw_text2'>지금 선거 해야 하는데 공약을 볼 시간이 없네...</p>
+            <p className='nw_text3'>내 성향에 맞는 후보를 쉽고 빠르게 찾고 싶어!</p>
+          </label>
+          { <div style={{height: '30px'}}>
+              <svg className='svg_button' width="24" height="16" viewBox="0 0 24 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M12.7715 15.0647C12.3715 15.5496 11.6285 15.5496 11.2285 15.0647L0.359201 1.88628C-0.178757 1.23404 0.28519 0.25 1.13066 0.25L22.8693 0.250002C23.7148 0.250002 24.1788 1.23405 23.6408 1.88629L12.7715 15.0647Z" fill="#84F5B8"></path>
+              </svg>
+            </div>
+          }
+          <div className='result_borde'>
+            <p> 1분만에 자신의 성향에 맞는 후보자를 추천해 줍니다 </p>
+            <p> 후보자들이 어떤 공약을 냈는지 한눈에 볼 수 있습니다. </p>
+          </div>
+          <div>
+            <img src={touchButton} alt='buttonImg' className='touch_button'/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Index;
--- client/src/Components/Nawoo/VotableItemList.js
+++ client/src/Components/Nawoo/VotableItemList.js
@@ -1,35 +1,37 @@
-import React from 'react';
 import { Link } from "react-router-dom";
+import Loding from '../Main/Loding';
 import "./VotableItemList.css";
 
-function VotableItemList({voteList}) {
+const VotableItemList = ({voteList}) => {
   return (
-    <div className='nawoo_form'>
-      <div className='nawoo_top'>
-        <div className='nawoo_logo'>
-          내가 원하는 후보상은?<br />
-          나만의 후보찾기
+    <div className='nw_form'>
+      <div className='nw_top'>
+        <div className='nw_explain1'>
+          <label className='nw_select_1'>나만의 후보찾기</label>
+          <label className='nw_select_2'>
+            <p className='nw_select_text1'>투표에도 인공지능을 </p>
+            <p className='nw_select_text2'>내 성향과 맞는 후보자를 찾고 싶을때</p>
+          </label>
         </div>
       </div>
-      <div className='nawoo_middle'>
-        <h2>투표 가능한 목록</h2>
-        <div className='vote-list'>  
-          {voteList.map((vote, index) => (
+      <div className='nw_middle'>
+        <div className='nw_votebox'>
+          <div className='nw_votebox_text'>
+            <p className='nw_select_text3'> 아래 투표들은 현재 학과에서 진행중인 투표입니다. </p>
+            <p className='nw_select_text3'>원하시는 투표를 선택하세요</p>
+          </div>
+          { !voteList ? <Loding/> : voteList.map((vote, index) => (
             <Link to={`CategorySelect/${voteList[index].voteCode}`} key={index}>
-              <div style={{height: '100px', width: '100%'}}>
+              <div className='nw_votelist'>
+                <label className='nw_vote'>
                 {vote.title}
+                </label>
               </div>
             </Link>
           ))}
         </div>
       </div>
-      <div className='nawoo_bottom'>
-        <div className='nawoo_logo'>
-          <button>다음</button>
-        </div>
-      </div>
     </div>
   );
 }
-
 export default VotableItemList;
