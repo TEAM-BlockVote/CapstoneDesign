6ea4d0605fc00484fe77691989b5b68aa061155e
jhchon <31531119+jhchon@users.noreply.github.com>
2023-09-14 23:36:56 +0900

    feat: 로딩 디자인 추가

---
+++ client/src/Components/Main/Loding.js
@@ -0,0 +1,18 @@
+import './Loding.css';
+
+const Loding = () => {
+  return(
+    <div className="loding_container">
+      <div className="loding_holder">
+        <div className="loding_box"/>
+      </div>
+      <div className="loding_holder">
+        <div className="loding_box"/>
+      </div>
+      <div className="loding_holder">
+        <div className="loding_box"/>
+      </div>
+    </div>  
+  )
+}
+export default Loding;
